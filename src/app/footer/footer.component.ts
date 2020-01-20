9b2a39df239dcd37f771d82b26af93aa6b8337f2
Anh Bui <anhbui159@gmail.com>
2020-01-20 11:51:55 -0500

    Anh bui commit

---
+++ src/app/footer/footer.component.ts
@@ -0,0 +1,37 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-footer',
+  templateUrl: './footer.component.html',
+  styleUrls: ['./footer.component.css']
+})
+export class FooterComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+
+
+  onNavigateGoogle() {
+    window.open("https://www.google.com", "_blank");
+  }
+
+  onNavigateLinkedIn() {
+    window.open("https://www.linkedin.com", "_blank");
+  }
+
+  onNavigateMonster() {
+    window.open("https://www.monster.com", "_blank");
+  }
+
+  onNavigateTwitter() {
+    window.open("https://www.twitter.com", "_blank");
+  }
+
+  onNavigateYoutube() {
+    window.open("https://www.youtube.com", "_blank");
+  }
+
+}
